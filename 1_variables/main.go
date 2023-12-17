9866fb9964e48a59751c81c1cb1ca2a33e5fb36d
Furkan COSGUN <furkan51cosgun@gmail.com>
2023-12-17 13:27:34 +0300

    Variables

---
+++ 1_variables/main.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var productname string
+	// var quantity int
+	// var discount float32
+	// var isInStock bool
+
+	// productname = "GoLang"
+	// quantity = 1
+	// discount = 1.0
+	// isInStock = true
+
+	// fmt.Println(productname)
+	// fmt.Println(productname, reflect.TypeOf(productname))
+
+	// fmt.Println(quantity, reflect.TypeOf(quantity))
+
+	// fmt.Println(discount, reflect.TypeOf(discount))
+
+	// fmt.Println(isInStock)
+	// fmt.Println(isInStock, reflect.TypeOf(isInStock))
+
+	productName := "GoLang"
+	fmt.Println(productName)
+
+	fmt.Printf("%v", productName) //Formatlama Ayrıntılı bilgi için go sayfasından bakabilirz
+
+}
